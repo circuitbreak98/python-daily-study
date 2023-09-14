a8489ec48eb1e765e9aecc5039ae47f5892602d0
circuitbreak98 <circuitbreak98@gmail.com>
2023-09-15 07:20:54 +0900

    add problem 46, 47

--- ex46.py
+++ ex46.py
@@ -1,4 +1,4 @@
-class Myenumerate():
+class Myenumerate1():
     def __init__(self, data):
         self.data = data
         self.index = 0
@@ -14,6 +14,33 @@ class Myenumerate():
         current_index = self.index 
         self.index += 1
         return (current_index, value)
+
+
+class Myenumerate():
+    def __init__(self, data):
+        self.data = data
+            
+    def __iter__(self):
+        return MyEnumerateIterator(self.data)
     
-for one_item in Myenumerate('abc'):
+
+class MyEnumerateIterator():
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+
+    def __next__(self):
+        if self.index >= len(self.data):
+            raise StopIteration
+        
+        value = self.data[self.index]
+        current_index = self.index 
+        self.index += 1
+        return (current_index, value)
+
+a = Myenumerate('abc')
+for one_item in a:
+    print(one_item)
+
+for one_item in a:
     print(one_item)
--- ex47.py
+++ ex47.py
@@ -1,15 +1,44 @@
-class CycleIterator:
-    def __next__(self):
-        #if self.count
-        if self.index >= len(self.data):
-            self.index = 0
-        
-        
-class Cycle:
-    def __init__(self, data):
+class Cycle1:
+    def __init__(self, data, count):
         self.data = data
-        self.index = 0
-        self.count = 0
+        self.count = count
     
     def __iter__(self):
-        return CycleIterator(self)
+        return CycleIterator(self.data, self.count)
+    
+
+class CycleIterator:
+    def __init__(self, data, count):
+        self.data = data
+        self.index = 0
+        self.count = count
+        self.current_count = 0
+
+    def __next__(self):
+        #if self.count
+        if self.current_count == self.count:
+            raise StopIteration
+        
+        elif self.index >= len(self.data):
+            self.index = 0
+
+        value = self.data[self.index]
+        self.index+=1
+        self.current_count+=1
+        return value
+
+
+class Circle1():
+
+    def __init__(self, data, max_times):
+        self.data = data
+        self.max_times = max_times
+
+    def __iter__(self):
+        n = len(self.data)
+        return (self.data[x%n] for x in range(self.max_times))
+
+    
+
+c = Circle1('abc', 10)
+print(list(c))
